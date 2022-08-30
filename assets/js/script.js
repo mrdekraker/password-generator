0e254ec42b5ef44e88246e799399058937ae1e12
mrdekraker <30578794+mrdekraker@users.noreply.github.com>
2022-08-29 20:52:16 -0500

    added character prompts

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 // TODO: Assignment code here
 function generatePassword() {
+    // //Set up length input
     const passwordLengthInput = window.prompt(`How long would you like your password to be? (8 -128)`);
 
     const passwordLength = parseInt(passwordLengthInput);
@@ -11,6 +12,25 @@ function generatePassword() {
     if (passwordLength < 8 || passwordLength > 128) {
         window.alert(`This input is outside the input parameters. Please input a number between 8 - 128.`);
     }
+
+    // //Set up prompts
+    const includeNumbers = window.confirm(`Would you like numbers in your password?`);
+    const includeLowercase = window.confirm(`Would you like lowercase in your password?`);
+    const includeUppercase = window.confirm(`Would you like uppercase in your password?`);
+    const includeSymbols = window.confirm(`Would you like symbols in your password?`);
+
+    // Start process over if everything is declined
+    if (
+        includeNumbers === false ||
+        includeLowercase === false ||
+        includeUppercase === false ||
+        includeSymbols === false
+    ) {
+        window.alert(`Please choose at least one of the options to generate your password.`);
+        generatePassword();
+    }
+
+    // SET UP GENERATOR FUNCTIONS
 }
 // Get references to the #generate element
 const generateBtn = document.querySelector('#generate');
