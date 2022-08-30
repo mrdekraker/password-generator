0d3d8a8c0fed5644a72cbf2992d9f71b15fb7e92
mrdekraker <30578794+mrdekraker@users.noreply.github.com>
2022-08-30 06:07:20 -0500

    added generator functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,36 +1,63 @@
 // TODO: Assignment code here
+
 function generatePassword() {
     // //Set up length input
     const passwordLengthInput = window.prompt(`How long would you like your password to be? (8 -128)`);
 
     const passwordLength = parseInt(passwordLengthInput);
 
-    if (isNaN(passwordLength)) {
+    if (Number.isNaN(passwordLength)) {
         window.alert(`This input is not a number! Please input a number between 8 - 128.`);
+        return;
     }
-
     if (passwordLength < 8 || passwordLength > 128) {
         window.alert(`This input is outside the input parameters. Please input a number between 8 - 128.`);
+        return;
     }
 
     // //Set up prompts
-    const includeNumbers = window.confirm(`Would you like numbers in your password?`);
     const includeLowercase = window.confirm(`Would you like lowercase in your password?`);
     const includeUppercase = window.confirm(`Would you like uppercase in your password?`);
+    const includeNumbers = window.confirm(`Would you like numbers in your password?`);
     const includeSymbols = window.confirm(`Would you like symbols in your password?`);
 
-    // Start process over if everything is declined
-    if (
-        includeNumbers === false ||
-        includeLowercase === false ||
-        includeUppercase === false ||
-        includeSymbols === false
-    ) {
-        window.alert(`Please choose at least one of the options to generate your password.`);
-        generatePassword();
+    // SET UP GENERATOR FUNCTIONS
+    function getRandomLower() {
+        return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+    }
+    function getRandomUpper() {
+        return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+    }
+    function getRandomNumber() {
+        return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+    }
+    function getRandomSymbol() {
+        const symbols = ` ~!@#$%%^&*()_+{}[]?.<>;:-=_+`;
+        return symbols[Math.floor(Math.random() * symbols.length)];
     }
 
-    // SET UP GENERATOR FUNCTIONS
+    const randomChar = [];
+
+    if (includeLowercase === true) {
+        randomChar.push(getRandomLower());
+    }
+
+    if (includeUppercase === true) {
+        randomChar.push(getRandomUpper());
+    }
+
+    if (includeNumbers === true) {
+        randomChar.push(getRandomNumber());
+    }
+    if (includeSymbols === true) {
+        randomChar.push(getRandomSymbol());
+    }
+    if (randomChar.length === 0) {
+        randomChar.push(getRandomLower());
+    }
+
+    const generatedPassword = ``;
+    console.log(randomChar);
 }
 // Get references to the #generate element
 const generateBtn = document.querySelector('#generate');
