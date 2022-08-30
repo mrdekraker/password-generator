9c711d8ee4afbead9a969b07cb7d566c1f6d92a8
mrdekraker <30578794+mrdekraker@users.noreply.github.com>
2022-08-29 20:40:00 -0500

    added first input question

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,19 @@
 // TODO: Assignment code here
+function generatePassword() {
+    const passwordLengthInput = window.prompt(`How long would you like your password to be? (8 -128)`);
 
-// TODO: Get references to the #generate element
-const generateBtn = document.querySelector('#generate');
+    const passwordLength = parseInt(passwordLengthInput);
 
-function onClick(e) {
-    generateBtn.addEventListener(`click`, e) = 
-        console.log(`Very click. Much wow.`);
+    if (isNaN(passwordLength)) {
+        window.alert(`This input is not a number! Please input a number between 8 - 128.`);
+    }
 
+    if (passwordLength < 8 || passwordLength > 128) {
+        window.alert(`This input is outside the input parameters. Please input a number between 8 - 128.`);
+    }
 }
-
-onClick();
+// Get references to the #generate element
+const generateBtn = document.querySelector('#generate');
 
 // Write password to the #password input
 function writePassword() {
@@ -20,34 +24,4 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-// generateBtn.addEventListener('click', writePassword);
-
-// const password = document.querySelector(`password`);
-
-// TODO: Generator Functions
-// function getRandomLower() {
-//     return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
-// }
-// function getRandomUpper() {
-//     return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
-// }
-// function getRandomNumber() {
-//     return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
-// }
-// function getRandomSymbol() {
-//     const symbols = ` ~!@#$%%^&*()_+{}[]?.<>;:-=_+`;
-//     return symbols[Math.floor(Math.random() * symbols.length)];
-// }
-
-// ! SET functions to randomize
-// const randomize = {
-//     lower: getRandomLower,
-//     upper: getRandomUpper,
-//     number: getRandomNumber,
-//     symbol: getRandomSymbol,
-// };
-
-// console.log(getRandomLower());
-// console.log(getRandomUpper());
-// console.log(getRandomNumber());
-// console.log(getRandomSymbol());
+generateBtn.addEventListener('click', writePassword);
